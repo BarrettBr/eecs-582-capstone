2ec50b2b820871c5a2118a4abeda871449f65f1c
BarrettBr <barrettbrown2012@gmail.com>
2026-02-15 03:04:27 -0600

    Added storing of data into a struct and then parsing it into json
    Optimized the storing / writing by storing a writer and the struct itself into the loop as to allow overwriting the data living there instead of re-creating a new object each time

--- ingest/internal/ingest/modbus_loop.go
+++ ingest/internal/ingest/modbus_loop.go
@@ -1,9 +1,11 @@
 package ingest
 
 import (
+	"bytes"
 	"context"
 	"encoding/binary"
 	"encoding/json"
+	"fmt"
 	"log"
 	"time"
 
@@ -12,10 +14,13 @@ import (
 )
 
 type ModbusLoop struct {
-	client   modbus.Client
-	queries  *database.Queries
-	interval time.Duration
-	mwBase   uint16
+	client      modbus.Client
+	queries     *database.Queries
+	interval    time.Duration
+	mwBase      uint16
+	sample      TempSample
+	jsonBuf     bytes.Buffer  // Writer that stores bytes for logging
+	jsonEncoder *json.Encoder // Converts struct -> Json
 }
 
 func NewModbusLoop(queries *database.Queries, interval time.Duration, address string, mwBase uint16) *ModbusLoop {
@@ -28,15 +33,18 @@ func NewModbusLoop(queries *database.Queries, interval time.Duration, address st
 
 	client := modbus.NewClient(handler)
 	if interval <= 0 {
-		interval = 500 * time.Second
+		interval = 5 * time.Second
 	}
 
-	return &ModbusLoop{
+	loop := &ModbusLoop{
 		client:   client,
 		queries:  queries,
 		interval: interval,
 		mwBase:   mwBase,
 	}
+	loop.jsonEncoder = json.NewEncoder(&loop.jsonBuf)
+	loop.jsonEncoder.SetEscapeHTML(false) // Shouldn't matter atm due to us sending int / uints back but a later protection
+	return loop
 }
 
 func (m *ModbusLoop) Run(ctx context.Context) error {
@@ -45,7 +53,7 @@ func (m *ModbusLoop) Run(ctx context.Context) error {
 
 	for {
 		if err := m.handleTick(ctx); err != nil {
-			log.Printf("modbus ingest tick failed: %v", err)
+			log.Printf("Modbus ingest tick failed: %v", err)
 		}
 
 		select {
@@ -66,33 +74,49 @@ func (m *ModbusLoop) handleTick(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	id := binary.BigEndian.Uint16(results[0:2])
-	typeCode := binary.BigEndian.Uint16(results[2:4])
-	sensorNum := binary.BigEndian.Uint16(results[4:6])
-	fanOnU := binary.BigEndian.Uint16(results[6:8])
-
-	tempX10 := int16(binary.BigEndian.Uint16(results[8:10]))
-	heaterX100 := int16(binary.BigEndian.Uint16(results[10:12]))
-
-	s := TempSample{
-		ID:           id,
-		SensorType:   sensorTypeFromCode(typeCode),
-		SensorNumber: sensorNum,
-		FanOn:        fanOnU != 0,
-		Temperature:  float64(tempX10) / 10.0,
-		HeaterPower:  float64(heaterX100) / 100.0,
+	if err := m.dataValidaterAndNormalizer(results); err != nil {
+		return err
 	}
 
-	b, _ := json.Marshal(s)
-	log.Printf("sample=%s", b)
+	m.jsonBuf.Reset()
+	if err := m.jsonEncoder.Encode(&m.sample); err != nil {
+		return fmt.Errorf("Encode sample json: %w", err)
+	}
+
+	// Print for now
+	payload := m.jsonBuf.Bytes()
+	log.Printf("Sample=%s", payload)
 	return nil
 }
 
-func sensorTypeFromCode(code uint16) string {
+func (m *ModbusLoop) dataValidaterAndNormalizer(results []byte) error {
+	// 2 bytes per register * 6 registers = 12 bytes.
+	if len(results) != 12 {
+		return fmt.Errorf("Invalid size: got %d bytes", len(results))
+	}
+
+	sensorCode := binary.BigEndian.Uint16(results[2:4])
+	sensorType, err := sensorTypeFromCode(sensorCode)
+	if err != nil {
+		return err
+	}
+
+	// Later change to better fit the actual dataset properly this is mainly filler till we swap to simulink to get data flowing
+	m.sample.ID = binary.BigEndian.Uint16(results[0:2])
+	m.sample.SensorType = sensorType
+	m.sample.SensorNumber = binary.BigEndian.Uint16(results[4:6])
+	m.sample.FanOn = binary.BigEndian.Uint16(results[6:8]) == 1
+	m.sample.Temperature = float64(int16(binary.BigEndian.Uint16(results[8:10]))) / 10.0
+	m.sample.HeaterPower = float64(binary.BigEndian.Uint16(results[10:12])) / 100.0
+
+	return nil
+}
+
+func sensorTypeFromCode(code uint16) (string, error) {
 	switch code {
 	case 1:
-		return "temperature_control_system"
+		return "temperature_control_system", nil
 	default:
-		return "unknown"
+		return "", fmt.Errorf("Invalid sensor type: %d", code)
 	}
 }
