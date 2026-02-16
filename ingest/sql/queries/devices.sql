5fcd65828be02de4640e6d2c7b4646ecca9fe832
Alexander Phibbs <alex.r.phibbs@gmail.com>
2026-02-15 23:23:33 -0600

    Add devices table migration and sqlc queries for tempsample

--- ingest/sql/queries/devices.sql
+++ ingest/sql/queries/devices.sql
@@ -1,8 +1,13 @@
--- name: CreateDevice :exec
-INSERT INTO devices (id, name)
-VALUES (?, ?);
+-- name: InsertTempSample :exec
+INSERT INTO temp_samples (
+    timestamp, sensor_type, sensor_number, fan_on, temperature, heater_power, anomalies
+) VALUES (?, ?, ?, ?, ?, ?, ?);
 
--- name: GetDevice :one
-SELECT id, name, created_at
-FROM devices
+-- name: GetTempSampleByID :one
+SELECT id, timestamp, sensor_type, sensor_number, fan_on, temperature, heater_power, anomalies
+FROM temp_samples
 WHERE id = ?;
+
+-- name: GetAllTempSamples :many
+SELECT id, timestamp, sensor_type, sensor_number, fan_on, temperature, heater_power, anomalies
+FROM temp_samples;
